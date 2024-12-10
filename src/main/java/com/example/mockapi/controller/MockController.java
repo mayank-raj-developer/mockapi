904cee502262e67b84aabadbd34c986e1b81953d
Mayank Raj <mayankraj2k@gmail.com>
2024-12-10 23:23:01 +0530

    Feign Implementation

--- src/main/java/com/example/mockapi/controller/MockController.java
+++ src/main/java/com/example/mockapi/controller/MockController.java
@@ -1,13 +1,19 @@
 package com.example.mockapi.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MockController {
     @GetMapping("/sample")
-    public String sample()
+    public JsonNode sample() throws  JsonProcessingException
     {
-        return "Hello Mock Api";
+        String temp="{\"message\": \"Hello Feign\"}";
+        ObjectMapper objectMapper=new ObjectMapper();
+        return objectMapper.readTree(temp);
     }
 }
